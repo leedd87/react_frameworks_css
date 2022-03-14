1e8f8bd3e0024334c28f6bd77de59481c8f31aed
leedd87 <leedav87@gmail.com>
2022-03-14 14:38:05 +0900

    React frameworks CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MaterialUi from "./components/MaterialUi";
+// import Bootstrap from "./components/Bootstrap";
+// import ReactBootstrap from "./components/ReactBootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Frameworks CSS con React</h1>
+			{/* <Bootstrap /> */}
+			{/* <ReactBootstrap /> */}
+			<MaterialUi />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/MaterialUi.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MaterialCard from "./MaterialCard";
+import MaterialDrawer from "./MaterialDrawer";
+
+export default function MaterialUi() {
+	return (
+		<>
+			<h2>Material UI</h2>
+			<MaterialCard />
+			<MaterialDrawer />
+		</>
+	);
+}
